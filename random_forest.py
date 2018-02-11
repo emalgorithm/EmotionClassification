59204c03b9913f7f048362a15f4c6be754c2317b
Emanuele Rossi <emanuele.rossi1909@gmail.com>
2018-02-11 14:04:46 +0000

    Random forest

---
+++ random_forest.py
@@ -0,0 +1,32 @@
+import random
+import numpy as np
+from tree import Tree
+
+class RandomForest(object):
+    def __init__(self, num_of_trees = 100):
+        self.trees = []
+        self.num_of_trees = num_of_trees
+
+    # It receives a binarized training dataset
+    def fit(self, predictors, X, y):
+        for i in range(self.num_of_trees):
+            sample_X, sample_y = self.get_new_sample(X, y)
+            tree = Tree()
+            tree.fit(predictors, sample_X, sample_y)
+            self.trees.append(tree)
+
+    def predict(self, data_point):
+        positive_confidence = sum([tree.predict(data_point)[1] for tree in self.trees]) / len(self.trees)
+        prediction = 1 if positive_confidence >= 0.5 else 0
+
+        return prediction, 1 - positive_confidence
+
+    def get_new_sample(self, X, y):
+        sample_X, sample_y = [], [] 
+
+        for i in range(len(X)):
+            index = random.randint(0, len(X) - 1)
+            sample_X.append(X[index])
+            sample_y.append(y[index])
+
+        return np.array(sample_X), np.array(sample_y)
